d41fdf58f0f5ce2c0883710211691658053323e4
John Feng <atjohnfeng@gmail.com>
2022-01-24 00:14:40 -0800

    Fix index.js issues and add cors to server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
+import "./App.css";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:3000");
+const socket = io.connect("http://localhost:4000");
 
 function App() {
   return (
     <div className="App">
-        Hello! Welcome.
+        Hello! Welcome. 
     </div>
   );
 }
