bc3bdf44ad1e9f46e778280613fcf77ddb72b9a0
John Feng <atjohnfeng@gmail.com>
2022-01-24 14:00:25 -0800

    Add front page form and fix backend index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
 import "./App.css";
+import { useState } from 'react';
 import io from "socket.io-client";
 
 const socket = io.connect("http://localhost:4000");
 
-function App() {
+const App = () => {
+
+  // useState hook to save username and room
+  const [username, setUser] = useState("");
+  const [room, setRoom] = useState("");
+
+  // Join room function - user is only allowed to join room if username / ID is
+  // filled in.
+  const joinRoom = () => {
+    if (username.length > 0 && room.length > 0) {
+
+    }
+  }
+
   return (
     <div className="App">
-        OuiTube is coming soon.
+        <h1>OuiTube</h1>
+        <h3>Enter a Room</h3>
+        <input type="text" placeholder="Name" />
+        <br />
+        <input type="text" placeholder="Room ID" />
+        <br />
+        <button>Enter</button>
     </div>
   );
 }
---
+++ frontend/src/startPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StartPage = (props) => (
+    <div>
+        HELLO.
+    </div>
+)
+
+export default StartPage;
