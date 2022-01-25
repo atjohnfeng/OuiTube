afee6dab743cb1f16d3d22cbebd05d4971004873
John Feng <atjohnfeng@gmail.com>
2022-01-24 20:49:53 -0800

    Add cors permission in vercel.json

--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', environment);
 
+    res.setHeader('Access-Control-Allow-Origin', 'https://oui-tube-backend.vercel.app/');
+
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
