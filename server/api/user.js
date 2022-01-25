666e1e945b82a491c2950be981e1ade2ae844d57
John Feng <atjohnfeng@gmail.com>
2022-01-24 22:13:01 -0800

    Add dependencies to root

--- server/api/user.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-    if (req.method === "GET") {
-        const users = "hello";
-        res.status(200).json({ users });
-    } else {
-        res.status(404).json({ status: "Error route note found" });
-    }
-};
